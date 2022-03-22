b44a9a3ee9a4079ce75ac5d6b21e44db01c78ffe
Vladimir Boika <vaverkax@gmail.com>
2022-03-22 17:27:56 +0300

    Initial Commit

---
+++ iOS_prof_HT4/iOS_prof_HT4App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_prof_HT4App.swift
+//  iOS_prof_HT4
+//
+//  Created by Владимир Скоробогатько on 22.03.22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_prof_HT4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
