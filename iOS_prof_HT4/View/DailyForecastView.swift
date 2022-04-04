bfa86b1a3d9f0d2ba24311d4b8d33dc722a014ab
Vladimir Boika <vaverkax@gmail.com>
2022-04-04 11:12:25 +0300

    added another detail screen

--- iOS_prof_HT4/View/DailyForecastView.swift
+++ iOS_prof_HT4/View/DailyForecastView.swift
@@ -15,45 +15,46 @@ struct DailyForecastView: View {
     @EnvironmentObject var routeModel: NavigationControllerViewModel
     
     var body: some View {
-        VStack {
-            if let coordinate = parentForecast.lattLong {
-                MapView(coordinate: coordinate.parseToCoordinates())
-                    .ignoresSafeArea(edges: .top)
-                    .frame(height: 300)
-                    .onTapGesture {
-                        routeModel.push(screenView: FullScreenMapView(coordinate: coordinate.parseToCoordinates()).toAnyView())
+        ScrollView {
+            VStack {
+                if let coordinate = parentForecast.lattLong {
+                    MapView(coordinate: coordinate.parseToCoordinates())
+                        .ignoresSafeArea(edges: .top)
+                        .frame(height: 300)
+                    VStack(alignment: .leading) {
+                        Text(parentForecast.title ?? "City")
+                            .font(.title)
+                        
+                        HStack {
+                            Text("Min temp: \(dailyForecast.minTemp ?? 0.0)")
+                            Spacer()
+                            Text("Max temp: \(dailyForecast.maxTemp ?? 0.0)")
+                        }
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        
+                        Divider()
+                        
+                        Text("On a \(dailyForecast.applicableDate ?? "Today")")
+                            .font(.title2)
+                        Text(dailyForecast.weatherStateName ?? "Weather")
                     }
-                VStack(alignment: .leading) {
-                    Text(parentForecast.title ?? "City")
-                        .font(.title)
-                    
-                    HStack {
-                        Text("Min temp: \(dailyForecast.minTemp ?? 0.0)")
-                        Spacer()
-                        Text("Max temp: \(dailyForecast.maxTemp ?? 0.0)")
-                    }
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-                    
-                    Divider()
-                    
-                    Text("On a \(dailyForecast.applicableDate ?? "Today")")
-                        .font(.title2)
-                    Text(dailyForecast.weatherStateName ?? "Weather")
+                    .padding()
+                    Spacer()
+                    Button("To full detail", action: {
+                        routeModel.push(screenView: DetatilDailyForecastView(dailyForecast: dailyForecast, parentForecast: parentForecast).toAnyView())
+                    })
+                } else {
+                    EmptyView()
                 }
-                .padding()
-                
-                Spacer()
-            } else {
-                EmptyView()
             }
-        }
-        .toolbar {
-            ToolbarItem(placement: .bottomBar) {
-                Button("Back") {
-                    routeModel.pop()
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Back") {
+                        routeModel.pop()
+                    }
+                    .font(.title)
                 }
-                .font(.title)
             }
         }
     }
